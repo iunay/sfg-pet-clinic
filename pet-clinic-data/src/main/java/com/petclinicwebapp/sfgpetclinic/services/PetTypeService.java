2f59b441024368e6b61940b46d7fcaf1872e0c40
iunay <ilary.poggi@gmail.com>
2020-12-04 22:21:45 +0100

    add PetTypeService map implementation. closes #29

---
+++ pet-clinic-data/src/main/java/com/petclinicwebapp/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.petclinicwebapp.sfgpetclinic.services;
+
+import com.petclinicwebapp.sfgpetclinic.models.PetType;
+
+public interface PetTypeService  extends CrudService<PetType,Long>{
+}
