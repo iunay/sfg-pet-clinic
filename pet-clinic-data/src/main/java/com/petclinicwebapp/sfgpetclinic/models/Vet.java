bfe648a76abfcaf63f4f0161514595b4a42eaaa2
iunay <ilary.poggi@gmail.com>
2020-12-16 11:29:03 +0100

    updated Vet JPA entity mappings( with Specialties entities  ). closes 37

--- pet-clinic-data/src/main/java/com/petclinicwebapp/sfgpetclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/com/petclinicwebapp/sfgpetclinic/models/Vet.java
@@ -1,11 +1,16 @@
 package com.petclinicwebapp.sfgpetclinic.models;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-private Set<Speciality> specialties = new HashSet<>();
+    @ManyToMany
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialties() {
         return specialties;
